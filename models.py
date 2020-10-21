84c0ee4190c2369d5ced7c9e3ebe6cac5e6c2526
Abhishek Nigam <abhisheknigam1996@gmail.com>
2020-10-22 02:05:03 +0530

    Save tag and bookmark tag mapping in database

--- models.py
+++ models.py
@@ -1,13 +1,41 @@
+from typing import List
+
+
+class Tag:
+    def __init__(self, _id: str) -> None:
+        self._id = _id
+
+    def __str__(self) -> str:
+        return f"Tag({self._id})"
+
+    def __repr__(self) -> str:
+        return f"Tag({self._id})"
+
+
+class BookmarkTagMapping:
+    def __init__(self, bookmark_id: str, tag_id: str) -> None:
+        self.bookmark_id = bookmark_id
+        self.tag_id = tag_id
+
+    def __str__(self) -> str:
+        return f"BookmarkTagMapping({self.bookmark_id},{self.tag_id})"
+
+    def __repr__(self) -> str:
+        return f"BookmarkTagMapping({self.bookmark_id},{self.tag_id})"
+
+
 class Bookmark:
-    def __init__(self, _id: str, created_at: str, updated_at: str, link: str, title: str) -> None:
+    def __init__(self, _id: str, created_at: str, updated_at: str, link: str, title: str, tags: List[Tag], mappings: List[BookmarkTagMapping]) -> None:
         self._id = _id
         self.created_at = created_at
         self.updated_at = updated_at
         self.link = link
         self.title = title
+        self.tags = tags
+        self.mappings = mappings
 
     def __str__(self) -> str:
-        return f"Bookmark({self._id},{self.title})"
+        return f"Bookmark({self._id},{self.title},{len(self.tags)} tags)"
 
     def __repr__(self) -> str:
-        return f"Bookmark({self._id},{self.title})"
+        return f"Bookmark({self._id},{self.title},{len(self.tags)} tags)"
--- run.py
+++ run.py
@@ -6,9 +6,9 @@ import sys
 import psycopg2
 import dateutil.parser
 from datetime import datetime
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Set, Tuple
 from dotenv import load_dotenv
-from models import Bookmark
+from models import Bookmark, BookmarkTagMapping, Tag
 from db import DatabaseConnection
 from config import (
     get_config,
@@ -23,7 +23,7 @@ MAX_BOOKMARKS_PER_PAGE = 50
 NETWORK_MAX_TRIES = 6
 
 
-def save_bookmarks_json(bookmarks: List[Bookmark], file_path: str) -> None:
+def save_as_json(bookmarks: List[Bookmark], tags: List[Tag], bookmark_tag_mappings: List[BookmarkTagMapping], file_path: str) -> None:
     save_location = os.path.join(
         "output", f"all_bookmarks_{int(time.time())}.json")
     if file_path:
@@ -32,6 +32,8 @@ def save_bookmarks_json(bookmarks: List[Bookmark], file_path: str) -> None:
     with open(save_location, "w") as out_file:
         json.dump({
             'bookmarks': [vars(bookmark) for bookmark in bookmarks],
+            'tags': [tag._id for tag in tags],
+            'bookmark_tag_mappings': [(mapping.bookmark_id, mapping.tag_id) for mapping in bookmark_tag_mappings],
             "created_at": datetime.utcnow().isoformat()
         }, out_file)
 
@@ -53,34 +55,66 @@ def get_most_recent_bookmark_created_at_datetime(connection) -> Optional[datetim
     return None
 
 
-def save_bookmarks_db(connection, bookmarks: List[Bookmark], most_recent_created_at_datetime: Optional[datetime]) -> None:
+def get_filtered_bookmarks(bookmarks: List[Bookmark], most_recent_created_at_datetime: datetime):
+    return list(filter(lambda x: dateutil.parser.parse(
+        x.created_at).replace(tzinfo=None) > most_recent_created_at_datetime, bookmarks))
 
-    filtered_bookmarks = bookmarks
-    if most_recent_created_at_datetime:
-        filtered_bookmarks = list(filter(lambda x: dateutil.parser.parse(
-            x.created_at).replace(tzinfo=None) > most_recent_created_at_datetime, bookmarks))
 
-    len_filtered_bookmarks = len(filtered_bookmarks)
-    if len_filtered_bookmarks > 0:
-        print(f"Saving {len_filtered_bookmarks} bookmarks in database")
+def save_in_db(connection, bookmarks: List[Bookmark]) -> None:
+    tag_records_set: Set[str] = set()
+    bookmark_records:  List[Tuple[str, str, str, str, str]] = []
+    bookmark_tag_mapping_records: List[Tuple[str, str]] = []
+
+    len_bookmarks = len(bookmarks)
+    if len_bookmarks > 0:
+        print(f"Saving {len_bookmarks} bookmarks in database")
     else:
         print("No new bookmarks to save in database")
         return
 
-    records = [(bookmark._id, bookmark.created_at, bookmark.updated_at,
-                bookmark.link, bookmark.title) for bookmark in filtered_bookmarks]
+    for bookmark in bookmarks:
+        bookmark_records.append((bookmark._id, bookmark.created_at, bookmark.updated_at,
+                                 bookmark.link, bookmark.title))
+        bookmark_tag_mapping_records.extend(
+            [(mapping.bookmark_id, mapping.tag_id) for mapping in bookmark.mappings])
+        tag_records_set.update([tag._id for tag in bookmark.tags])
+
+    tag_records = [(tag,) for tag in tag_records_set]
 
     with DatabaseConnection.Cursor(connection) as cursor:
         try:
-            sql_upsert_query = """
+            sql_bookmark_upsert_query = """
                             INSERT INTO bookmark (_id, created_at, updated_at, link, title)
                             VALUES (%s,%s,%s,%s,%s)
                             ON CONFLICT (_id) DO UPDATE SET updated_at = EXCLUDED.updated_at, link = EXCLUDED.link, title = EXCLUDED.title
                             """
-            cursor.executemany(sql_upsert_query, records)
+            cursor.executemany(sql_bookmark_upsert_query, bookmark_records)
             connection.commit()
         except (Exception, psycopg2.Error) as error:
-            print("Failed inserting record into mobile table {}".format(error))
+            print("Failed inserting records in bookmark table {}".format(error))
+
+        try:
+            sql_tag_insert_query = """
+                            INSERT INTO tag (_id)
+                            VALUES (%s)
+                            ON CONFLICT (_id) DO NOTHING
+                            """
+            cursor.executemany(sql_tag_insert_query, tag_records)
+            connection.commit()
+        except (Exception, psycopg2.Error) as error:
+            print("Failed inserting record in tag table {}".format(error))
+
+        try:
+            sql_tag_mapping_record_insert_query = """
+                            INSERT INTO bookmark_tag_mapping (bookmark_id, tag_id)
+                            VALUES (%s,%s)
+                            ON CONFLICT (bookmark_id, tag_id) DO NOTHING
+                            """
+            cursor.executemany(sql_tag_mapping_record_insert_query, bookmark_tag_mapping_records)
+            connection.commit()
+        except (Exception, psycopg2.Error) as error:
+            print(
+                "Failed inserting records in bookmark tag mapping table {}".format(error))
 
 
 def get_bookmark_from_dict(item: Dict) -> Bookmark:
@@ -91,7 +125,39 @@ def get_bookmark_from_dict(item: Dict) -> Bookmark:
         link=item['link'], title=item['title'])
 
 
-def get_bookmarks(token: str, most_recent_bookmark_created_at_datetime: Optional[datetime]):
+def get_models_from_api_response_items(items: List[Dict]) -> Tuple[List[Bookmark], List[Tag], List[BookmarkTagMapping]]:
+    bookmarks: List[Bookmark] = []
+    tags_set: Set[str] = set()
+    bookmark_tag_mappings: List[BookmarkTagMapping] = []
+
+    for item in items:
+        items_tags: List[Tag] = []
+        mappings: List[BookmarkTagMapping] = []
+
+        for item_tag_str in item['tags']:
+            bookmark_tag_mappings.append(
+                BookmarkTagMapping(item['_id'], item_tag_str)
+            )
+            mappings.append(BookmarkTagMapping(item['_id'], item_tag_str))
+            items_tags.append(Tag(item_tag_str))
+
+        bookmarks.append(
+            Bookmark(
+                _id=item['_id'],
+                created_at=item['created'],
+                updated_at=item['lastUpdate'],
+                link=item['link'],
+                title=item['title'],
+                tags=items_tags,
+                mappings=mappings)
+        )
+
+        tags_set.update(item['tags'])
+
+    return bookmarks, [Tag(tag) for tag in tags_set], bookmark_tag_mappings
+
+
+def get_api_response_items(token: str, most_recent_bookmark_created_at_datetime: Optional[datetime]):
     pages_to_skip = 0
     items = []
 
@@ -123,14 +189,13 @@ def get_bookmarks(token: str, most_recent_bookmark_created_at_datetime: Optional
                         items_data[-1]['created']).replace(tzinfo=None)
 
                     if most_recent_bookmark_created_at_datetime >= last_record_created_at_datetime:
-                        return [get_bookmark_from_dict(item)
-                                for item in items]
+                        return items
             else:
-                return [get_bookmark_from_dict(item)
-                        for item in items]
+                return items
         else:
             print(f"Request failed with status code {response.status_code}")
             return None
+
 
 def wait_for_network_connection(max_tries: int) -> None:
     wait_for_seconds = 10
@@ -140,7 +205,8 @@ def wait_for_network_connection(max_tries: int) -> None:
 
     while True:
         if no_of_tries > max_tries:
-            print(f"Couldn't get network connection after {max_tries} attempts, exiting")
+            print(
+                f"Couldn't get network connection after {max_tries} attempts, exiting")
             sys.exit(1)
 
         try:
@@ -149,10 +215,12 @@ def wait_for_network_connection(max_tries: int) -> None:
         except (requests.ConnectionError, requests.Timeout) as e:
             pass
 
-        print(f"Waiting for internet connection. Next attempt in {wait_for_seconds} seconds")
+        print(
+            f"Waiting for internet connection. Next attempt in {wait_for_seconds} seconds")
         time.sleep(wait_for_seconds)
         wait_for_seconds = wait_for_seconds * 2
         no_of_tries = no_of_tries + 1
+
 
 def main():
     load_dotenv()
@@ -172,16 +240,26 @@ def main():
                     most_recent_bookmark_created_at_datetime = get_most_recent_bookmark_created_at_datetime(
                         conn)
 
-                bookmarks = get_bookmarks(
+                api_response_items = get_api_response_items(
                     token, most_recent_bookmark_created_at_datetime)
-                if bookmarks:
-                    save_bookmarks_db(
-                        conn, bookmarks, most_recent_bookmark_created_at_datetime)
+                if api_response_items:
+                    bookmarks, _, _ = get_models_from_api_response_items(
+                        api_response_items)
+
+                    if most_recent_bookmark_created_at_datetime:
+                        filtered_bookmarks = get_filtered_bookmarks(
+                            bookmarks, most_recent_bookmark_created_at_datetime)
+                        save_in_db(
+                            conn, filtered_bookmarks)
+                    else:
+                        save_in_db(conn, bookmarks)
 
     elif args.save == BACKUP_LOCATION_JSON:
-        bookmarks = get_bookmarks(token, None)
-        if bookmarks:
-            save_bookmarks_json(bookmarks, args.file)
+        api_response_items = get_api_response_items(token, None)
+        if api_response_items:
+            bookmarks, tags, bookmark_tag_mappings = get_models_from_api_response_items(
+                api_response_items)
+            save_as_json(bookmarks, tags, bookmark_tag_mappings, args.file)
 
 
 if __name__ == "__main__":
